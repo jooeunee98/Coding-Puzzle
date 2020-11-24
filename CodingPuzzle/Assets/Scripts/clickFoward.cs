fe86cb7448e07a807632dd11a9894250ba28ca54
jooeunee98 <jooeunee98@naver.com>
2020-11-25 06:48:11 +0900

    command to array

---
+++ CodingPuzzle/Assets/Scripts/clickFoward.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class clickFoward : MonoBehaviour
+{
+    public GameObject array;
+    int indexNo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickF()
+    {
+        indexNo = array.GetComponent<commandList>().index;
+        array.GetComponent<commandList>().command[indexNo] = 1;
+        array.GetComponent<commandList>().index++;
+    }
+}
---
+++ CodingPuzzle/Assets/Scripts/clickRight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickRight : MonoBehaviour
+{
+    public GameObject array;
+    int indexNo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickR()
+    {
+        indexNo = array.GetComponent<commandList>().index;
+        array.GetComponent<commandList>().command[indexNo] = 2;
+        array.GetComponent<commandList>().index++;
+    }
+}
