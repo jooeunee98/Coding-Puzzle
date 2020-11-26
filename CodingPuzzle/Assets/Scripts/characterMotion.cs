d560e24777f9ee0ca909f71dcc23a0c33b5879ba
HAN-GIIL <62708300+HAN-GIIL@users.noreply.github.com>
2020-11-27 04:26:36 +0900

    20201125

--- CodingPuzzle/Assets/Scripts/characterMotion.cs
+++ CodingPuzzle/Assets/Scripts/characterMotion.cs
@@ -5,60 +5,65 @@ using UnityEngine;
 public class characterMotion : MonoBehaviour
 {
     private Animator animator;
-
     public GameObject array;
     public int comNum = 0;
-    public int[] commandLoaded;
     private void Awake()
     {
-        // Awake 필요가 없네
-        // comNum = array.GetComponent<commandList>().command.Length;
-        
-        // animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
     }
+
+    public int[] commandLoaded;
     private IEnumerator coroutine;
     bool go_forward = false;
     bool turn_right = false;
     bool turn_left = false;
-    float stRotationY;
     int turnDegree = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
         comNum = array.GetComponent<commandList>().command.Length;
-        
-        animator = GetComponent<Animator>();
-        coroutine = readCommand(1.0f);
+        commandLoaded = new int[comNum];
+        for(int i=0; i<comNum; i++){
+            commandLoaded[i] = array.GetComponent<commandList>().command[i];
+        }
+        coroutine = readCommand(3.0f);
         StartCoroutine(coroutine);
 
     }
 
     public IEnumerator readCommand(float waitTime){
         for(int i=0; i<commandLoaded.Length; i++){
+            
             if(commandLoaded[i] == 1){
                 animator.SetBool("idle", false);
                 animator.SetBool("run", true);
                 animator.SetBool("turn", false);
                 waitTime = 1.0f;
                 go_forward = true;
+                turn_left = false;
+                turn_right = false;
             } else if(commandLoaded[i] == 2){
+                if(i==1)
                 animator.SetBool("idle", false);
                 animator.SetBool("run", false);
                 animator.SetBool("turn", true);
                 go_forward = false;
                 turn_right = true;
-                turnDegree = 0;
-                // 도는건 얼마 안걸리니까 waitTime -> 1 로 변경
-                waitTime = 2f;
+                turn_left = false;
+                waitTime = 3f;
             } else if(commandLoaded[i] == 3){
                 animator.SetBool("idle", false);
                 animator.SetBool("run", false);
                 animator.SetBool("turn", true);
                 go_forward = false;
                 turn_left = true;
-                turnDegree = 0;
-                // 도는건 얼마 안걸리니까 waitTime -> 1 로 변경
-                waitTime = 2f;
+                turn_right = false;
+                waitTime = 3f;
+            } else {
+                go_forward = false;
+                turn_left = false;
+                turn_right = false;
             }
             yield return new WaitForSeconds(waitTime);
 
@@ -67,26 +72,40 @@ public class characterMotion : MonoBehaviour
         animator.SetBool("run", false);
         animator.SetBool("turn", false);
         go_forward = false;
+        turn_left = false;
+        turn_right = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log("commandLoaded Length : " + commandLoaded.Length);
+        comNum = array.GetComponent<commandList>().command.Length;
+
+        //Debug.Log(comNum);
+
         for(int i=0; i<comNum; i++){
-            commandLoaded[i] = array.GetComponent<commandList>().command[i];
+           commandLoaded[i] = array.GetComponent<commandList>().command[i];
+           Debug.Log(commandLoaded[i]);
         }
         if(go_forward == true){
             transform.Translate(this.transform.localRotation * Vector3.forward * Time.deltaTime * 25, Space.World);
         }
         else if(turn_right == true){
             if(turnDegree < 90){
-                gameObject.transform.Rotate(0,2,0);
-                turnDegree+=2;
-             } 
+                Debug.Log("turnDegree : " + turnDegree);
+                gameObject.transform.Rotate(0,5,0);
+                turnDegree+=5;
+             } else if(turnDegree == 90){
+                 Debug.Log("Ok by here");
+                 turnDegree = 0;
+                 turn_right = false;
+             }
         } else if(turn_left == true){
-            if(turnDegree < 90){
-                gameObject.transform.Rotate(0,-2,0);
-                turnDegree+=2;
+            if(turnDegree <= 90){
+                gameObject.transform.Rotate(0,-5,0);
+                turnDegree+=5;
              } 
 
         } 
