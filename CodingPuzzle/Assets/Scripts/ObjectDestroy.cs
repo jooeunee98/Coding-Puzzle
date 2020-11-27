8e381d02006536a4831bc0051f7a81cab32d265f
yoon2030 <55525539+yoon2030@users.noreply.github.com>
2020-11-27 23:05:16 +0900

    _2

---
+++ CodingPuzzle/Assets/Scripts/ObjectDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            Destroy(col.gameObject);
+            GameObject Player1 = Instantiate(Resources.Load("prefab/player")) as GameObject;
+            Player1.transform.position= new Vector3(0,68,50);
+            Player1.transform.localScale -= new Vector3(15, 15, 15);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
